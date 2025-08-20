3f90e665ca9c711c08fa650017b62bde80bc2e9a
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-08-19 23:31:36 -0400

    completed day 3

---
+++ bonus_projects/bonus3.py
@@ -0,0 +1,14 @@
+meals = ["pizza", "pasta", "drink"]
+
+for char in "meals":
+    print(char.capitalize())
+
+
+list_of_strings = ["string1", "string2", "string3"]
+
+for char in list_of_strings:
+    print(char)
+
+
+for i in range(0, 10):
+    print(i)
--- todo_app/main.py
+++ todo_app/main.py
@@ -5,6 +5,9 @@ todos = []
 # while statement to repeat question
 while True:
     user_action = input("Type add, show, or exit: ")
+    user_action = (
+        user_action.strip()
+    )  # to remove any whitespace accidently added by the user
 
     match user_action:
         case "add":
@@ -12,6 +15,7 @@ while True:
             todos.append(todo)
         case "show":
             for item in todos:
+                item = item.title()
                 print(item)
         case "exit":
             break
