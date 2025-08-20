20f9d7aa427940a6251c0f389d0762d130199896
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-08-20 11:43:38 -0400

    added edit feature

--- todo_app/main.py
+++ todo_app/main.py
@@ -4,7 +4,7 @@ todos = []
 
 # while statement to repeat question
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, or exit: ")
     user_action = (
         user_action.strip()
     )  # to remove any whitespace accidently added by the user
@@ -17,6 +17,12 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+
         case "exit":
             break
 
