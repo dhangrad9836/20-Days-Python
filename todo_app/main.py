4fbcb667af4462216cc0082f521a011c34b6fafe
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-08-19 23:07:36 -0400

    changed out to for loop and use .strip() to remove any whitespace added accidently by user

--- todo_app/main.py
+++ todo_app/main.py
@@ -11,7 +11,8 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
-            print(todos)
+            for item in todos:
+                print(item)
         case "exit":
             break
 
