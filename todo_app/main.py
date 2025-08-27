1a4301c80e592523344258d504a527caa1ca7cad
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-08-27 10:33:15 -0400

    added deleting function

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,10 +1,10 @@
 # the text inside parentheses is called the arguement
-
+# list we are storing todos in
 todos = []
 
 # while statement to repeat question
 while True:
-    user_action = input("Type add, show, edit, or exit: ")
+    user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = (
         user_action.strip()
     )  # to remove any whitespace accidently added by the user
@@ -18,7 +18,8 @@ while True:
             for index, item in enumerate(todos):
                 item = item.title()
                 index = index + 1  # to start the index at 1 instead of 0
-                print(index, " ", item)
+                # print(index, " ", item)
+                print(f"{index}-{item}")
         case "edit":
             number = int(input("Number of the todo to edit: "))
             # we need to subtract 1 from number so the index is correct
@@ -26,6 +27,9 @@ while True:
             new_todo = input("Enter new todo: ")
             # update the todo by overwriting the todo at that index
             todos[number] = new_todo
+        case "complete":
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)  # subtract 1 to get correct index
 
         case "exit":
             break
