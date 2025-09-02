0d0b3fe239df62547632ddd5cd55794cfb77932c
Darren Dhanpat <darren.dhanpat@yahoo.com>
2025-09-02 11:59:18 -0400

    added bonus6 for manually creating text files

---
+++ bonus_projects/bonus6.py
@@ -0,0 +1,11 @@
+contents = [
+    "All carrots to be sliced longitudinally",
+    "The carrots were reported sliced",
+    "The carrots were sliced again",
+]
+
+filenames = ["doc.txt", "report.txt", "presentation.txt"]
+
+for content, filename in zip(contents, filenames):
+    file = open(f"./files/{filename}", "w")
+    file.write(content)
